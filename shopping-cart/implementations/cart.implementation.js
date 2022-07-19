f50257380497d1ee4c47400784eea85bb659f0a6
Yan Karlo Teixeira <yan.karlo29@gmail.com>
2022-07-19 11:02:47 -0300

    Impllements the gRPC server configuration

---
+++ shopping-cart/implementations/cart.implementation.js
@@ -0,0 +1,27 @@
+const grpc = require('grpc');
+
+module.exports = {
+  async create(call, callback) {
+  },
+
+  async clean(call, callback) {
+  },
+
+  async getById(call, callback) {
+  },
+
+  async addItem(call, callback) {
+  },
+
+  async addCoupon(call, callback) {
+  },
+
+  async qtyUpdate(call, callback) {
+  },
+
+  async calculate(call, callback) {
+  },
+
+  async removeItem(call, callback) {
+  },
+};
