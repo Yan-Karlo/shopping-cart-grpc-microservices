09c9f6a9d4b6d74c4e3e7d9231b035b8a2c0dd66
Yan Karlo Teixeira <yan.karlo29@gmail.com>
2022-07-20 01:28:13 -0300

    Implements swagger basic set-up and fixing http status code Fixes the correponding between http1 and http2 (gRPC) for 400 http status code

--- api/app/routes/index.routes.js
+++ api/app/routes/index.routes.js
@@ -1,4 +1,6 @@
 const CartRoute = require('./v1/cart.routes');
+const swaggerUI = require('swagger-ui-express');
+const swaggerDocument = require('./documentation/swagger.json');
 
 module.exports = class IndexRoute {
   constructor(router) {
@@ -7,6 +9,7 @@ module.exports = class IndexRoute {
   }
 
   Initialize() {
+    this.router.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument))
     new CartRoute(this);
 
     return this.router;
