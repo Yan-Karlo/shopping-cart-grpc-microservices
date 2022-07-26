71ba7f5a20ff5ce0e3e231de47dcdfdd1696a31b
Yan Karlo Teixeira <yan.karlo29@gmail.com>
2022-07-26 14:49:42 -0300

    Implements the product desscription and invetory Includes the description and invetory information in the cart object struture whenever it is recovered

--- products-catalog/dependencies/server.dependencies.js
+++ products-catalog/dependencies/server.dependencies.js
@@ -3,7 +3,7 @@ module.exports = {
     url: 'localhost:3002',
     dns: 'mongodb://localhost:27017/product_db',
     protoPath: require('path').join(__dirname, '../../proto/product.proto'),
-    protoOptions :       {
+    protoOptions: {
       keepCase: true,
       longs: String,
       enums: String,
@@ -13,10 +13,10 @@ module.exports = {
     implementation: require('../implementations/product.implementation'),
   },
   docker: {
-    url: '0.0.0.0:3002', 
+    url: '0.0.0.0:3002',
     dns: 'mongodb://db/product_db',
     protoPath: require('path').join(__dirname, '../proto/product.proto'),
-    protoOptions :       {
+    protoOptions: {
       keepCase: true,
       longs: String,
       enums: String,
